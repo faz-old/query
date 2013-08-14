ed03b47f9e3e61ee367431102b58f8b27a7102ee
stornowski <stornowski@6c3b17df-ac5c-534a-b9b0-6e063f355c51>
2013-08-14 12:43:42 +0000

    PPUP-359 Suche nach allen Artikeln eines Autors auf Solr umstellen (Marginal-Autor-Box und Autorenseite)
    *Create proxy for all arguments from constructors to avoid null validation errors and problems with abstract classes
    
    git-svn-id: https://svn.faz.net/fem/Projekte/fazcore/trunk@30885 6c3b17df-ac5c-534a-b9b0-6e063f355c51

--- src/main/java/de/faz/modules/query/FieldDefinitionGenerator.java
+++ src/main/java/de/faz/modules/query/FieldDefinitionGenerator.java
@@ -15,191 +15,154 @@
 package de.faz.modules.query;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Stack;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 import net.sf.cglib.proxy.Callback;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /** @author Andreas Kaubisch <a.kaubisch@faz.de> */
 public class FieldDefinitionGenerator {
 
-    private static final Logger log = LoggerFactory.getLogger(FieldDefinitionGenerator.class);
+	private final Stack<FieldDefinition> fieldStack;
 
-    private final Stack<FieldDefinition> fieldStack;
+	public FieldDefinitionGenerator() {
+		fieldStack = new Stack<FieldDefinition>();
+	}
 
-    public FieldDefinitionGenerator() {
-        fieldStack = new Stack<FieldDefinition>();
-    }
+	public <T extends Mapping> T createFieldDefinition(final Class<T> mappingClass) {
+		return enhanceWithInterceptor(mappingClass, new MethodInterceptor() {
+			@Override
+			public Object intercept(final Object o, final Method method, final Object[] objects, final MethodProxy methodProxy)
+					throws Throwable {
+				String fieldName = null;
+				Integer boost = null;
 
-    public <T extends Mapping> T createFieldDefinition(final Class<T> mappingClass) {
-        return enhanceWithInterceptor(mappingClass, new MethodInterceptor() {
-            @Override
-            public Object intercept(final Object o, final Method method, final Object[] objects, final MethodProxy methodProxy)
-                    throws Throwable {
-                String fieldName = null;
-                Integer boost = null;
+				final MapToField mapping = method.getAnnotation(MapToField.class);
+				if (mapping != null) {
+					fieldName = mapping.value();
+				}
 
-                final MapToField mapping = method.getAnnotation(MapToField.class);
-                if (mapping != null) {
-                    fieldName = mapping.value();
-                }
+				final BoostResult boostResult = method.getAnnotation(BoostResult.class);
+				if (boostResult != null) {
+					boost = boostResult.value();
+				}
 
-                final BoostResult boostResult = method.getAnnotation(BoostResult.class);
-                if (boostResult != null) {
-                    boost = boostResult.value();
-                }
+				if (fieldName != null) {
+					fieldStack.push(new FieldDefinition(fieldName, boost));
+				}
 
-                if (fieldName != null) {
-                    fieldStack.push(new FieldDefinition(fieldName, boost));
-                }
+				return null;
+			}
+		});
+	}
 
-                return null;
-            }
-        });
-    }
+	public <T extends Mapping> T enhanceWithInterceptor(final Class<T> enhancedClass, final Callback interceptor) {
+		return enhanceWithInterceptorInternal(enhancedClass, interceptor);
+	}
 
-    public <T extends Mapping> T enhanceWithInterceptor(final Class<T> enhancedClass, final Callback interceptor) {
-        final Enhancer enhancer = new Enhancer();
-        enhancer.setCallback(interceptor);
-        enhancer.setSuperclass(enhancedClass);
-        final Class<?>[] argumentTypes = getConstructorArgumentsFromClass(enhancedClass);
-        T proxiedObject;
-        if (argumentTypes == null) {
-            proxiedObject = (T) enhancer.create();
-        } else {
-            proxiedObject = (T) enhancer.create(argumentTypes, createInstances(argumentTypes));
-        }
-        return proxiedObject;
-    }
+	private <T extends Object> T enhanceWithInterceptorInternal(final Class<T> enhancedClass) {
+		return enhanceWithInterceptorInternal(enhancedClass, new MethodInterceptor() {
+			@Override
+			public Object intercept(final Object o, final Method method, final Object[] objects, final MethodProxy methodProxy) throws Throwable {
+				return null;
+			}
+		});
+	}
 
-    @Nonnull
-    private Object[] createInstances(@Nonnull final Class<?>[] clazzes) {
-        final int classesLength = clazzes.length;
-        final Object[] objects = new Object[classesLength];
-        for (int i = 0; i < classesLength; i++) {
-            final Class<?> clazz = clazzes[i];
-            final Class<?>[] constructorArgumentsFromClass = getConstructorArgumentsFromClass(clazz);
-            if (constructorArgumentsFromClass != null) {
-                if (constructorArgumentsFromClass.length > 0) {
-                    objects[i] = createObjectWithArgumentedConstructor(clazz, constructorArgumentsFromClass);
-                } else {
-                    objects[i] = createObjectWithDefaultConstructor(clazz);
-                }
-            } else {
-                objects[i] = createObjectWithDefaultConstructor(clazz);
-            }
+	private <T extends Object> T enhanceWithInterceptorInternal(final Class<T> enhancedClass, final Callback interceptor) {
+		final Enhancer enhancer = new Enhancer();
+		enhancer.setCallback(interceptor);
+		enhancer.setSuperclass(enhancedClass);
+		final Class<?>[] argumentTypes = getConstructorArgumentsFromClass(enhancedClass);
+		T proxiedObject;
+		if (argumentTypes == null) {
+			proxiedObject = (T) enhancer.create();
+		} else {
+			proxiedObject = (T) enhancer.create(argumentTypes, createInstances(argumentTypes));
+		}
+		return proxiedObject;
+	}
 
-        }
+	@Nonnull
+	private Object[] createInstances(@Nonnull final Class<?>[] clazzes) {
+		final Object[] objects = new Object[clazzes.length];
+		for (int i = 0; i < clazzes.length; i++) {
+			objects[i] = enhanceWithInterceptorInternal(clazzes[i]);
+		}
 
-        return objects;
-    }
+		return objects;
+	}
 
-    @Nullable
-    private Object createObjectWithArgumentedConstructor(@Nonnull final Class<?> clazz,
-            @Nonnull final Class<?>[] constructorArgumentsFromClass) {
-        Object object = null;
-        try {
-            final int length = constructorArgumentsFromClass.length;
-            final Object[] parameters = new Object[length];
-            for (int i = 0; i < length; i++) {
-                parameters[i] = null;
-            }
-            object = clazz.getDeclaredConstructor(constructorArgumentsFromClass).newInstance(parameters);
-        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-                | NoSuchMethodException | SecurityException e) {
-            log.warn("Could not create constructor for [{}] with arguments [{}]", clazz.getName(), constructorArgumentsFromClass,
-                    e);
-        }
-        return object;
-    }
+	public String getFieldNameOf(final Object fieldMethod) {
+		String fieldName = null;
+		if (isNotEmpty()) {
+			final FieldDefinition definition = pop();
+			fieldName = definition.name.toString();
+		}
 
-    @Nullable
-    private Object createObjectWithDefaultConstructor(@Nonnull final Class<?> clazz) {
-        Object object = null;
-        try {
-            object = clazz.newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
-            log.warn("Could not create default constructor for [{}]", clazz.getName(), e);
-        }
-        return object;
-    }
+		return fieldName;
+	}
 
-    public String getFieldNameOf(final Object fieldMethod) {
-        String fieldName = null;
-        if (isNotEmpty()) {
-            final FieldDefinition definition = pop();
-            fieldName = definition.name.toString();
-        }
+	public boolean isEmpty() {
+		return fieldStack.isEmpty();
+	}
 
-        return fieldName;
-    }
+	public boolean isNotEmpty() {
+		return !isEmpty();
+	}
 
-    public boolean isEmpty() {
-        return fieldStack.isEmpty();
-    }
+	public FieldDefinition pop() {
+		return fieldStack.pop();
+	}
 
-    public boolean isNotEmpty() {
-        return !isEmpty();
-    }
-
-    public FieldDefinition pop() {
-        return fieldStack.pop();
-    }
-
-    private Class<?>[] getConstructorArgumentsFromClass(final Class<?> cls) {
-        final Constructor<?>[] constructors = cls.getConstructors();
-        if (constructors != null && constructors.length > 0) {
-            return constructors[0].getParameterTypes();
-        }
-        return null;
-    }
+	private Class<?>[] getConstructorArgumentsFromClass(final Class<?> cls) {
+		final Constructor<?>[] constructors = cls.getConstructors();
+		if (constructors != null && constructors.length > 0) {
+			return constructors[0].getParameterTypes();
+		}
+		return null;
+	}
 }
 
 class FieldDefinition {
-    final CharSequence name;
-    final int boost;
+	final CharSequence name;
+	final int boost;
 
-    public FieldDefinition(final CharSequence name, final Integer boost) {
-        this.name = name;
-        if (boost != null) {
-            this.boost = boost;
-        } else {
-            this.boost = 1;
-        }
-    }
+	public FieldDefinition(final CharSequence name, final Integer boost) {
+		this.name = name;
+		if (boost != null) {
+			this.boost = boost;
+		} else {
+			this.boost = 1;
+		}
+	}
 
-    CharSequence getName() {
-        return name;
-    }
+	CharSequence getName() {
+		return name;
+	}
 
-    FieldDefinition setName(final String name) {
-        return new FieldDefinition(name, boost);
-    }
+	FieldDefinition setName(final String name) {
+		return new FieldDefinition(name, boost);
+	}
 
-    int getBoost() {
-        return boost;
-    }
+	int getBoost() {
+		return boost;
+	}
 
-    FieldDefinition setBoost(final int boost) {
-        return new FieldDefinition(name, boost);
-    }
+	FieldDefinition setBoost(final int boost) {
+		return new FieldDefinition(name, boost);
+	}
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (obj instanceof FieldDefinition) {
-            final FieldDefinition def = (FieldDefinition) obj;
-            return name.equals(def.name) && boost == def.boost;
-        }
-        return super.equals(obj);
-    }
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj instanceof FieldDefinition) {
+			final FieldDefinition def = (FieldDefinition) obj;
+			return name.equals(def.name) && boost == def.boost;
+		}
+		return super.equals(obj);
+	}
 }
--- src/test/java/de/faz/modules/query/FieldDefinitionGeneratorTest.java
+++ src/test/java/de/faz/modules/query/FieldDefinitionGeneratorTest.java
@@ -12,55 +12,96 @@ import org.junit.Test;
 /** @author Andreas Kaubisch <a.kaubisch@faz.de> */
 public class FieldDefinitionGeneratorTest {
 
-    FieldDefinitionGenerator generator;
+	FieldDefinitionGenerator generator;
 
-    @Before
-    public void setUp() {
-        generator = new FieldDefinitionGenerator();
-    }
+	@Before
+	public void setUp() {
+		generator = new FieldDefinitionGenerator();
+	}
 
-    @Test
-    public void enhance_withInheritedMapping_getFieldnameFromSuperClass() {
-        final InheritedMapping mapping = generator.createFieldDefinition(InheritedMapping.class);
-        assertEquals("field2", generator.getFieldNameOf(mapping.getField2()));
-    }
+	@Test
+	public void enhance_withInheritedMapping_getFieldnameFromSuperClass() {
+		final InheritedMapping mapping = generator.createFieldDefinition(InheritedMapping.class);
+		assertEquals("field2", generator.getFieldNameOf(mapping.getField2()));
+	}
 
-    @Test
-    public void enhance_withInheritedMappingWithNonDefaultConstructor_getFieldnameFromSuperClass() {
-        final InheritedMapping mapping = generator.createFieldDefinition(InheritedMappingWithNonDefaultConstructor.class);
-        assertEquals("field2", generator.getFieldNameOf(mapping.getField2()));
-    }
+	@Test
+	public void enhance_withInheritedMappingWithNonDefaultConstructor_getFieldnameFromSuperClass() {
+		final InheritedMapping mapping = generator.createFieldDefinition(InheritedMappingWithNonDefaultConstructor.class);
+		assertEquals("field2", generator.getFieldNameOf(mapping.getField2()));
+	}
 
-    @Test
-    public void enhance_withNativeMethod_returnsFieldnameOfNativeMethod() {
-        final InheritedMapping mapping = generator.createFieldDefinition(InheritedMapping.class);
-        assertEquals("field4", generator.getFieldNameOf(mapping.getField4()));
-    }
+	@Test
+	public void enhance_withNativeMethod_returnsFieldnameOfNativeMethod() {
+		final InheritedMapping mapping = generator.createFieldDefinition(InheritedMapping.class);
+		assertEquals("field4", generator.getFieldNameOf(mapping.getField4()));
+	}
+
+	@Test
+	public void enhance_withGenericInhaeritedMapping_returnsFieldnameOfNativeMethod() {
+		@SuppressWarnings("unchecked")
+		final InheritedMappingWithGenerics<ExtendedTestClass> inheritedMappingWithGenerics = generator.createFieldDefinition(InheritedMappingWithGenerics.class);
+		assertEquals("field1", generator.getFieldNameOf(inheritedMappingWithGenerics.getField1()));
+	}
+
+	@Test
+	public void enhance_withAbstractInhaeritedMapping_returnsFieldnameOfNativeMethod() {
+		@SuppressWarnings("unchecked")
+		final InheritedMappingWithAbstractGenerics<ExtendedAbstractTestClass> inheritedMappingWithGenerics = generator
+				.createFieldDefinition(InheritedMappingWithAbstractGenerics.class);
+		assertEquals("field1", generator.getFieldNameOf(inheritedMappingWithGenerics.getField1()));
+	}
 
 }
 
 class InheritedMapping extends TestMapping {
 
-    @MapToField("field3")
-    public String getField3() {
-        return null;
-    }
+	@MapToField("field3")
+	public String getField3() {
+		return null;
+	}
 
-    @MapToField("field4")
-    public boolean getField4() {
-        return false;
-    }
+	@MapToField("field4")
+	public boolean getField4() {
+		return false;
+	}
 }
 
 class InheritedMappingWithNonDefaultConstructor extends InheritedMapping {
 
-    public InheritedMappingWithNonDefaultConstructor(@Nonnull final ObjectWithNoDefaultConstructor object) {
-        Validate.notNull(object, "Object must not be null.");
-    }
+	public InheritedMappingWithNonDefaultConstructor(@Nonnull final ObjectWithNoDefaultConstructor object) {
+		Validate.notNull(object, "Object must not be null.");
+	}
 }
 
 class ObjectWithNoDefaultConstructor {
 
-    public ObjectWithNoDefaultConstructor(@Nullable final Object object) {
-    }
+	public ObjectWithNoDefaultConstructor(@Nullable final Object object) {
+	}
+}
+
+class InheritedMappingWithGenerics<T extends TestClass> extends TestMapping {
+
+	public InheritedMappingWithGenerics(@Nonnull final T object) {
+		Validate.notNull(object, "Object must not be null.");
+	}
+}
+
+class InheritedMappingWithAbstractGenerics<T extends AbstractTestClass> extends TestMapping {
+
+	public InheritedMappingWithAbstractGenerics(@Nonnull final T object) {
+		Validate.notNull(object, "Object must not be null.");
+	}
+}
+
+class ExtendedTestClass extends TestClass {
+}
+
+class TestClass {
+}
+
+class ExtendedAbstractTestClass extends AbstractTestClass {
+}
+
+abstract class AbstractTestClass {
 }
