45442b98a32c0cfe97515f03542627ba9e5db874
kaubisch <kaubisch@6c3b17df-ac5c-534a-b9b0-6e063f355c51>
2013-09-26 09:35:44 +0000

    PPUP-XXX fix wrong paging für series detail layout
    git-svn-id: https://svn.faz.net/fem/Projekte/fazcore/trunk@32522 6c3b17df-ac5c-534a-b9b0-6e063f355c51

--- src/test/java/de/faz/modules/query/QueryDefaultComparatorTest.java
+++ src/test/java/de/faz/modules/query/QueryDefaultComparatorTest.java
@@ -14,7 +14,6 @@
 package de.faz.modules.query;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Answers;
@@ -27,7 +26,6 @@ import static org.mockito.Mockito.verify;
 
 /** @author Andreas Kaubisch <a.kaubisch@faz.de> */
 @RunWith(MockitoJUnitRunner.class)
-@Ignore
 public class QueryDefaultComparatorTest {
     @Mock(answer = Answers.RETURNS_DEEP_STUBS) Query originalQuery;
     Query.Operator operator;
